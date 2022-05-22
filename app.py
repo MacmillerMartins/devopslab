7ea78f020f934f30530a5f76316a3096de8a6603
Macmiller <58778886+MacmillerMartins@users.noreply.github.com>
2022-05-22 00:29:35 -0300

    tentando desbugar

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def pagina_inicial():
     return "Desafio - Macmiller: Lab Cloud & DevOps"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
